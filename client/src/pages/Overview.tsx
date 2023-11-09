b1bc322d85d259fe687560e05adfc5fa1516ffdc
DENCYUPC\Dency <takotakocyuman@gmail.com>
2023-11-09 12:05:55 +0900

    fix baseTime on chart

--- client/src/pages/Overview.tsx
+++ client/src/pages/Overview.tsx
@@ -30,7 +30,7 @@ export default function Overview() {
 
     function convertToOverviewData(schemasData: SchemasSummaryData[]): OverviewData[] {
         return schemasData.map((data) => ({
-            name: formatBaseTime(new Date(data.base_time)),
+            name: formatBaseTime(new Date(data.base_time))+"~",
             INFO: data.infolog_count,
             WARNING: data.warninglog_count,
             ERROR: data.errorlog_count
@@ -45,10 +45,10 @@ export default function Overview() {
             console.error("Error fetching data: ", error);
         }
     };
-    
+
     useEffect(() => {
         fetchData();
-        
+
         // TODO: 5秒ごとにデータを更新する
         // const interval = setInterval(fetchData, 5000);
         // return () => clearInterval(interval);
@@ -69,13 +69,10 @@ export default function Overview() {
         switch (summary.latest_log.level_name) {
             case 'INFO':
                 return 'success';
-
             case 'WARNING':
                 return 'warning';
-
             case 'ERROR':
                 return 'danger';
-
             default:
                 return null;
         }
@@ -85,13 +82,10 @@ export default function Overview() {
         switch (levelName) {
             case 'INFO':
                 return 'NORMAL';
-
             case 'WARNING':
                 return 'WARNING';
-
             case 'ERROR':
                 return 'ERRORED';
-
             default:
                 return null;
         }
@@ -139,7 +133,7 @@ export default function Overview() {
             <div className="col-12 py-3 sm:px-3 lg:col-6 xl:col-4">
                 <div className="p-4 border-1 surface-border surface-card border-round">
                     <div className="flex flex-wrap align-items-center justify-content-between gap-2">
-                        <div className="flex align-items-center gap-2">
+                        <div className="flex align-items-center gap-2 py-2 border-round-sm cursor-pointer hover:surface-100">
                             <i className="pi pi-tag"></i>
                             <span className="font-semibold">{summary.category || 'Others'}</span>
                         </div>
@@ -148,7 +142,9 @@ export default function Overview() {
                     <div className="flex flex-column align-items-center gap-3 pt-5 pb-2">
                         <div className="text-2xl font-bold w-full">{summary.name}</div>
                         <OverviewChart data={convertToOverviewData(summary.data)} layout={overviewLayout} />
-                        <div className="w-full">
+                    </div>
+                    <div className="flex align-items-center justify-content-between flex-row">
+                        <div className="w-10 p-2 border-round-sm cursor-pointer hover:surface-100">
                             <h4 className="m-0 p-0 font-bold">最新取得ログ</h4>
                             <div className="text-base flex flex-wrap:wrap gap-2 align-items-baseline">
                                 <div>{formatDate(new Date(summary.latest_log.timestamp))}</div>
@@ -156,9 +152,7 @@ export default function Overview() {
                                 <div>({timeAgo(new Date(summary.latest_log.timestamp))})</div>
                             </div>
                         </div>
-                    </div>
-                    <div className="flex align-items-center justify-content-between flex-row-reverse">
-                        <Button className={buttonClassName} icon="pi pi-chart-bar" rounded onClick={redirectToDashboard}></Button>
+                        <Button className={buttonClassName} icon="pi pi-chart-bar" onClick={redirectToDashboard} rounded text severity="secondary"></Button>
                         <Tooltip target={`.${buttonClassName}`} content="Jump to DashBoard." position="left"/>
                     </div>
                 </div>
@@ -177,7 +171,7 @@ export default function Overview() {
 
     return (
         <div className="card">
-            <h1>System Overview</h1>
+            <h1 className="sm:px-3">System Overview</h1>
             <Tooltip target=".goToDashBoardButton" content="Jump to DashBoard." position="left"/>
             <DataView value={summary} itemTemplate={itemTemplate} layout={'grid'}/>
         </div>
